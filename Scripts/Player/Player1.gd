95a102112aaea834ac3f86ad760df6788398bf9e
théau Yapi <theau.yapi@ynov.com>
2024-05-11 20:19:06 +0200

    car with a gun - réecriture code player 1 et 2

--- Scripts/Player/Player1.gd
+++ Scripts/Player/Player1.gd
@@ -1,34 +1,38 @@
 extends KinematicBody2D
 
+# Signal emitted when player's health changes
 signal health_changed(current_health)
 
+# Maximum health of the player
 var MAX_HEALTH = 25
+# Current health of the player
 var current_health: int = MAX_HEALTH
 
+# Movement speed of the player
 var movespeed = 500
-# Add a variable to control rotation locking
-var lock_rotate = false
 
-#bullet var
+# Bullet variables
 var bullet_speed = 2000
 var bullet = preload("res://Scenes/bullet/bullet1.tscn")
 
-# Declare variables to control burst firing
+# Variables to control burst firing
 var burst_count = 0
 var burst_max = 7  # Number of bullets per burst
 var burst_cooldown = 3.0  # Cooldown between bursts
 var burst_timer = 0.0
 
+# Variable to store the last motion direction
+var last_motion_direction = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+# Process player actions and movements
 func _physics_process(delta):
 	var motion = Vector2()
-	var Player2 = get_parent().get_node("Player2")
-	
-	#player mvnt code
+
+	# Player movement code
 	if Input.is_action_pressed("up1"):
 		motion.y -= 1
 	if Input.is_action_pressed("down1"):
@@ -37,30 +41,34 @@ func _physics_process(delta):
 		motion.x -= 1
 	if Input.is_action_pressed("right1"):
 		motion.x += 1
-	if Input.is_action_pressed("rotate1"):
-		lock_rotate = true
-	else:
-		lock_rotate = false
-	
+
+	# Normalize the motion vector to ensure consistent movement speed in all directions
 	motion = motion.normalized()
+
+	# Move the player using move_and_slide
 	motion = move_and_slide(motion * movespeed)
-	# Lock rotation if enabled
-	if not lock_rotate:
-		look_at(Player2.position)
-	
+
+	# Update the last motion direction if the player is moving
+	if motion.length_squared() > 0:
+		last_motion_direction = motion
+
 	# Handle burst cooldown
 	if burst_timer > 0:
 		burst_timer -= delta
 	elif burst_count >= burst_max:
 		burst_count = 0
-	
 	if Input.is_action_just_pressed("fire1"):
 		fire()
-	
 	if Input.is_action_pressed("rafale1"):
 		rafale()
 
+	# Rotate the player based on the last motion direction
+	if last_motion_direction.length_squared() > 0:
+		rotation = last_motion_direction.angle()
+		rotation_degrees = rad2deg(rotation)
+		self.rotation_degrees = rotation_degrees
 
+# Fire a single bullet
 func fire():
 	var bullet_instance = bullet.instance()
 	bullet_instance.position = get_global_position()
@@ -68,7 +76,7 @@ func fire():
 	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
 	get_tree().get_root().call_deferred("add_child",bullet_instance)
 
-
+# Fire a burst of bullets
 func rafale():
 	if burst_count < burst_max:
 		var bullet_instance = bullet.instance()
@@ -77,18 +85,19 @@ func rafale():
 		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
 		get_tree().get_root().call_deferred("add_child",bullet_instance)
 		burst_count += 1
-	
 	if burst_count >= burst_max:
 		burst_timer = burst_cooldown
 
-
+# Reload the current scene (used for player respawn or restart)
 func kill():
 	get_tree().reload_current_scene()
 
-
+# Handle collision with bullets
 func _on_Area2D_body_entered(body):
+	# If player's health is depleted, reload the scene to restart the game
 	if "bullet2" in body.name and current_health <= 1:
 		kill()
+	# If player is hit by a bullet, decrement health and emit health_changed signal
 	elif "bullet2" in body.name:
 		current_health -= 1
-	emit_signal("health_changed", current_health)
+		emit_signal("health_changed", current_health)
--- Scripts/Player/Player2.gd
+++ Scripts/Player/Player2.gd
@@ -1,13 +1,17 @@
 extends KinematicBody2D
 
+# Signal emitted when player's health changes
 signal health_changed(new_health)
 
-const MAX_HEALTH = 25
-var current_health = MAX_HEALTH
+# Maximum health of the player
+var MAX_HEALTH = 25
+# Current health of the player
+var current_health: int = MAX_HEALTH
 
+# Movement speed of the player
 var movespeed = 500
-var lock_rotate = false
-#bullet var
+
+# Bullet variables
 var bullet_speed = 2000
 var bullet = preload("res://Scenes/bullet/bullet2.tscn")
 
@@ -17,15 +21,18 @@ var burst_max = 7  # Number of bullets per burst
 var burst_cooldown = 3.0  # Cooldown between bursts
 var burst_timer = 0.0
 
+# Variable to store the last motion direction
+var last_motion_direction = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
+# Process player actions and movements
 func _physics_process(delta):
 	var motion = Vector2()
-	var Player1 = get_parent().get_node("Player1")
-	#player mvnt code
+
+	# Player movement code
 	if Input.is_action_pressed("up2"):
 		motion.y -= 1
 	if Input.is_action_pressed("down2"):
@@ -34,17 +41,17 @@ func _physics_process(delta):
 		motion.x -= 1
 	if Input.is_action_pressed("right2"):
 		motion.x += 1
-	if Input.is_action_pressed("rotate1"):
-		lock_rotate = true
-	else:
-		lock_rotate = false
-	
+
+	# Normalize the motion vector to ensure consistent movement speed in all directions
 	motion = motion.normalized()
+
+	# Move the player using move_and_slide
 	motion = move_and_slide(motion * movespeed)
-	# Lock rotation if enabled
-	if not lock_rotate:
-		look_at(Player1.position)
-	
+
+	# Update the last motion direction if the player is moving
+	if motion.length_squared() > 0:
+		last_motion_direction = motion
+
 	# Handle burst cooldown
 	if burst_timer > 0:
 		burst_timer -= delta
@@ -57,8 +64,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("rafale2"):
 		rafale()
 
+	# Rotate the player based on the last motion direction
+	if last_motion_direction.length_squared() > 0:
+		rotation = last_motion_direction.angle()
+		rotation_degrees = rad2deg(rotation)
+		self.rotation_degrees = rotation_degrees
 
-
+# Fire a single bullet
 func fire():
 	var bullet_instance = bullet.instance()
 	bullet_instance.position = get_global_position()
@@ -66,7 +78,7 @@ func fire():
 	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
 	get_tree().get_root().call_deferred("add_child",bullet_instance)
 
-
+# Fire a burst of bullets
 func rafale():
 	if burst_count < burst_max:
 		var bullet_instance = bullet.instance()
@@ -79,14 +91,16 @@ func rafale():
 	if burst_count >= burst_max:
 		burst_timer = burst_cooldown
 
-	
+# Reload the current scene (used for player respawn or restart)
 func kill():
 	get_tree().reload_current_scene()
 
-
+# Handle collision with bullets
 func _on_Area2D_body_entered(body):
+	# If player's health is depleted, reload the scene to restart the game
 	if "bullet1" in body.name and current_health <= 1:
 		kill()
+	# If player is hit by a bullet, decrement health and emit health_changed signal
 	elif "bullet1" in body.name:
 		current_health -= 1
-	emit_signal("health_changed", current_health)
+		emit_signal("health_changed", current_health)
