08c38f49c914dfe534ae27ed59c914ddedef3ba8
théau Yapi <theau.yapi@ynov.com>
2024-05-15 11:16:51 +0200

    eofjof

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -15,9 +15,5 @@ func _on_instructions_pressed():
 	get_tree().change_scene("res://Scenes/UI/instruction.tscn")
 
 
-func _on_settings_pressed():
-	get_tree().change_scene("res://Scenes/UI/Settings.tscn")
-
-
 func _on_quit_pressed():
 	get_tree().quit()
